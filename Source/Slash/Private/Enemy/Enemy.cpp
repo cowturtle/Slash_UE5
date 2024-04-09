52ea44dcf5380d924f0b35a7208187641f3eeb6a
KeXin <theodorickeithlim@gmail.com>
2024-04-09 21:56:56 +0800

    133. Playing the Hit React Montage

--- Source/Slash/Private/Enemy/Enemy.cpp
+++ Source/Slash/Private/Enemy/Enemy.cpp
@@ -24,6 +24,17 @@ void AEnemy::BeginPlay()
 	
 }
 
+void AEnemy::PlayHitReactMontage(const FName& SectionName)
+{
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+	if (AnimInstance && HitReactMontage)
+	{
+		AnimInstance->Montage_Play(HitReactMontage);
+		//AnimInstance->Montage_SetPlayRate(EquipMontage, 1.5f);
+		AnimInstance->Montage_JumpToSection(SectionName, HitReactMontage);
+	}
+}
+
 void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -39,5 +50,6 @@ void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 void AEnemy::GetHit(const FVector& ImpactPoint)
 {
 	DRAW_SPHERE_COLOR(ImpactPoint, FColor::Orange);
+	PlayHitReactMontage(FName("FromLeft"));
 }
 
--- Source/Slash/Public/Enemy/Enemy.h
+++ Source/Slash/Public/Enemy/Enemy.h
@@ -5,6 +5,9 @@
 #include "Interfaces/HitInterface.h"
 #include "Enemy.generated.h"
 
+
+class UAnimMontage;
+
 UCLASS()
 class SLASH_API AEnemy : public ACharacter, public IHitInterface
 {
@@ -13,13 +16,17 @@ class SLASH_API AEnemy : public ACharacter, public IHitInterface
 public:
 	AEnemy();
 
-protected:
-	virtual void BeginPlay() override;
-
-public:	
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	virtual void GetHit(const FVector& ImpactPoint) override;
+protected:
+	virtual void BeginPlay() override;
+
+	void PlayHitReactMontage(const FName& SectionName);
+private:
+	UPROPERTY(EditDefaultsOnly, Category = Montages)
+	UAnimMontage* HitReactMontage;
+	
 };
