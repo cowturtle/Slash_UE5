efe4d75397de09f81233857e8d743be2a4493b69
KeXin <theodorickeithlim@gmail.com>
2024-04-14 14:54:11 +0800

    145. Breakable Actor

---
+++ Source/Slash/Private/BreakableActor.cpp
@@ -0,0 +1,29 @@
+#include "BreakableActor.h"
+#include "GeometryCollection/GeometryCollectionComponent.h"
+
+ABreakableActor::ABreakableActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	GeometryCollection = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("GeometryCollection"));
+	SetRootComponent(GeometryCollection);
+	GeometryCollection->SetGenerateOverlapEvents(true);
+	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+}
+
+void ABreakableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABreakableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABreakableActor::GetHit(const FVector& ImpactPoint)
+{
+}
+
