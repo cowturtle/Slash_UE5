2c9361cd3f234f3bfa52ce554f470157e2b45311
KeXin <theodorickeithlim@gmail.com>
2024-04-30 14:41:16 +0800

    203. Slash Overlay

---
+++ Source/Slash/Public/HUD/SlashOverlay.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SlashOverlay.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SLASH_API USlashOverlay : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+
+	void SetHealthBarPercent(float Percent);
+	void SetStaminaBarPercent(float Percent);
+	void SetGold(int32 Gold);
+	void SetSouls(int32 Souls);
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* HealthProgressBar;
+
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* StaminaProgressBar;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* GoldText;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* SoulsText;
+};
