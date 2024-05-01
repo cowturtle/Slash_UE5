89b58835b36ea8e98341e8fece4f42f6a9b54c80
KeXin <theodorickeithlim@gmail.com>
2024-05-01 14:39:05 +0800

    214. Soul Pickup Effects

--- Source/Slash/Private/Items/Soul.cpp
+++ Source/Slash/Private/Items/Soul.cpp
@@ -9,6 +9,8 @@ void ASoul::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Ot
 	{
 		PickupInterface->AddSouls(this);
 	}
+	SpawnPickupSystem();
+	SpawnPickupSound();
 
 	Destroy();
 }
--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -5,6 +5,7 @@
 #include "Item.generated.h"
 
 class USphereComponent;
+class UNiagaraComponent;
 
 enum class EItemState : uint8
 {
@@ -44,6 +45,9 @@ protected:
 	UFUNCTION()
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
+	virtual void SpawnPickupSystem();
+	virtual void SpawnPickupSound();
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	UStaticMeshComponent* ItemMesh;
 
@@ -53,10 +57,17 @@ protected:
 	USphereComponent* Sphere;
 
 	UPROPERTY(EditAnywhere)
-	class UNiagaraComponent* ItemEffect;
+	UNiagaraComponent* ItemEffect;
+
+	UPROPERTY(EditAnywhere)
+	USoundBase* PickupSound;
+
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
+
+	UPROPERTY(EditAnywhere)
+	class UNiagaraSystem* PickupEffect;
 };
 
 template<typename T>
