8e3cfef0dbe761d96f39bfd523bf756f16b5fc0a
KeXin <theodorickeithlim@gmail.com>
2024-05-04 11:04:31 +0800

    227. Insect

--- Source/Slash/Public/Characters/SlashCharacter.h
+++ Source/Slash/Public/Characters/SlashCharacter.h
@@ -57,7 +57,7 @@ private:
 	void Disarm();
 	void Arm();
 	void PlayEquipMontage(const FName& SectionName);
-	virtual void Die() override;
+	virtual void Die_Implementation() override;
 	bool HasEnoughStamina();
 	bool IsOccupied();
 
